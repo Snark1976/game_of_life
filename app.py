3219d8f5326f4e208fc7b42e9632a8df30a4eb2e
Snark1976 <kohovec@gmail.com>
2021-09-12 08:50:57 +0300

    add changes of size and generations per second

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask import render_template
 from game_of_life import GameOfLife
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['get', 'post'])
+@app.route('/index', methods=['get', 'post'])
 def index():
-    GameOfLife(25, 25)
-    return render_template('index.html')
+    if request.method == 'POST':
+        vert = int(request.form.get('vert'))
+        horiz = int(request.form.get('horiz'))
+        pts = int(request.form.get('pts'))
+    else:
+        vert = 25
+        horiz = 30
+        pts = 1
+    GameOfLife(horiz, vert, pts=pts)
+    return render_template('index.html', vert=vert, goriz=horiz, pts=pts)
 
 @app.route('/live')
 def live():
@@ -17,7 +26,7 @@ def live():
     if game.counter:
         game.form_new_generation()
     game.counter += 1
-    return render_template('live.html', world=game.world, counter=game.counter)
+    return render_template('live.html', game=game)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
